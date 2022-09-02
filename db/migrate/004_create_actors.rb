1fc4e3eb3ed03edd14370733b93e5feb583f141a
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-09-02 15:11:03 +0300

    commit tree 01063d18839798161f619210647b7bba39bdf4be

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,11 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
+    # create_table :actors do |t|
+    #   t.string:first_name
+    #   t.string:last_name
     create_table :actors do |t|
-      t.string:first_name
-      t.string:last_name
+      t.string :first_name
+      t.string :last_name
   end
 end
 end
